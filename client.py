7de9e89bd685777c2a1b77d6c4dddc3c95ec5514
Wraith <97615989+WR117H@users.noreply.github.com>
2023-07-01 17:59:03 +0330

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,15 @@
 import socket
-import time
-import sys
 import threading
+from module import banner
 import argparse
 from colorama import Fore
-from module import banner
-parser = argparse.ArgumentParser(description='A script to make chatrooms with encryption')
-parser.add_argument('--host', type=str, default='127.0.0.1', help='Host address to start the server on')
-parser.add_argument('--port', type=int, default=9999, help='Port number of the chatroom server')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', type=str, default='127.0.0.1', help='Host address to connect to')
+parser.add_argument('--port', type=int, default=9999, help='Port number of the host server')
 parser.add_argument('--user', type=str, help='Username for the chatroom')
 
 args = parser.parse_args()
-
 
 username = args.user
 port = args.port
@@ -27,27 +25,17 @@ class ChatroomClient:
         global username
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.host, self.port))
-        if username == None:
-           username = input("Enter your username: ")
-           self.client_socket.sendall(username.encode("utf-8"))
-
+        if username is None:
+            username = input("Enter your username: ")
+            self.client_socket.sendall(username.encode("utf-8"))
         else:
-           self.client_socket.sendall(username.encode("utf-8"))
+            self.client_socket.sendall(username.encode("utf-8"))
         receive_thread = threading.Thread(target=self.receive_messages)
         receive_thread.start()
 
         while True:
             message = input()
-
-            if message.lower() == "/quit":
-                self.client_socket.sendall(message.encode("utf-8"))
-                self.client_socket.close()
-                break
-
-
-            else:
-
-                self.client_socket.sendall(message.encode("utf-8"))
+            self.client_socket.sendall(message.encode("utf-8"))
 
     def receive_messages(self):
         while True:
@@ -55,16 +43,13 @@ class ChatroomClient:
                 message = self.client_socket.recv(1024).decode("utf-8")
                 if message:
                     sender_username, _, message_content = message.partition(":")
-                    if sender_username != "<You>":
-                        print(message.replace("<You> ", ""))
+                    print(Fore.LIGHTBLUE_EX + sender_username + Fore.RESET + " > " + message_content)
             except Exception as e:
                 print(f"Error receiving message: {e}")
                 self.client_socket.close()
                 break
 
 if __name__ == "__main__":
-   
-    banner.ban()
+    banner.ban(0)
     client = ChatroomClient(ip_address, port)
     client.start()
-   
