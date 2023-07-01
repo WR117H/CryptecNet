7e23b6594d4a53a2d89ef089220197db02e7bdad
Wraith <97615989+WR117H@users.noreply.github.com>
2023-07-01 18:14:07 +0330

    Update banner.py

--- module/banner.py
+++ module/banner.py
@@ -1,15 +1,22 @@
-import os
 from colorama import Fore
 
-banner="""
-    ____                  ________          __ 
-   / __ \___  ___  ____  / ____/ /_  ____ _/ /_
-  / / / / _ \/ _ \/ __ \/ /   / __ \/ __ `/ __/
- / /_/ /  __/  __/ /_/ / /___/ / / / /_/ / /_  
-/_____/\___/\___/ .___/\____/_/ /_/\__,_/\__/  
-               /_/                             
+github = "https://github.com/wR117H/CryptetNet"
+wraith = "@wR117H"
+
+banner=f"""
+   ______                 __            __  _   __     __ 
+  / ____/______  ______  / /____  _____/ /_/ | / /__  / /_
+ / /   / ___/ / / / __ \/ __/ _ \/ ___/ __/  |/ / _ \/ __/
+/ /___/ /  / /_/ / /_/ / /_/  __/ /__/ /_/ /|  /  __/ /_  
+\____/_/   \__, / .___/\__/\___/\___/\__/_/ |_/\___/\__/  
+          /____/_/ {Fore.RESET+wraith}                                   
 """
-def ban():
+def ban(x):
    os.system('clear||cls')
    print(Fore.LIGHTMAGENTA_EX+banner+Fore.RESET)
-   
+   if x == 0:
+
+      print("Documentaiton "+Fore.LIGHTYELLOW_EX+github+Fore.RESET>
+   else:
+      print("Documentaiton "+Fore.LIGHTYELLOW_EX+github+Fore.RESET)
+
